8e2aa8673d8ba3b7f18fc644c0249f3f220a67c5
Nick Provost <nickprovs@hotmail.com>
2019-04-03 00:04:50 -0400

    Splash screen... Replaces splash.png with a transparent, more square like thing next so you're optimized for more devices and can change background color on the fly.

--- Nickprovs.Albatross/Nickprovs.Albatross.Android/MainActivity.cs
+++ Nickprovs.Albatross/Nickprovs.Albatross.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Nickprovs.Albatross.Droid
 {
-    [Activity(Label = "Nickprovs.Albatross", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Nickprovs.Albatross", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
