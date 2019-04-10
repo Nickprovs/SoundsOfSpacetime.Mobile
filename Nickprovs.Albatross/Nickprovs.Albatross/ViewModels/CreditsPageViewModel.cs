46b50865d668f2be1ee5ba14bd72f555bba06d88
Nick Provost <nickprovs@hotmail.com>
2019-04-09 21:29:08 -0400

    Initial Credit's Page

--- Nickprovs.Albatross/Nickprovs.Albatross/ViewModels/CreditsPageViewModel.cs
+++ Nickprovs.Albatross/Nickprovs.Albatross/ViewModels/CreditsPageViewModel.cs
@@ -1,7 +1,11 @@
-﻿using Prism.Navigation;
+﻿using Prism.Commands;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
 
 namespace Nickprovs.Albatross.ViewModels
 {
@@ -15,6 +19,7 @@ namespace Nickprovs.Albatross.ViewModels
 
         #region Properties
 
+        public DelegateCommand ContactDeveloperCommand { get; }
 
         #endregion
 
@@ -23,12 +28,37 @@ namespace Nickprovs.Albatross.ViewModels
         public CreditsPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             this.Title = "Credits";
+            this.ContactDeveloperCommand = new DelegateCommand(this.OnContactDeveloper);
         }
 
         #endregion
 
         #region Private Methods
 
+        private void OnContactDeveloper()
+        {
+            this.SendDeveloperEmail();
+        }
+
+        private void SendDeveloperEmail()
+        {
+            string shareurl = String.Empty;
+            String messageBody = "Replace this text with a comment or issue you wish to bring to the developer's attention.";
+            String messageTitle = "Sounds of Spacetime App Mail";
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                var subject = Regex.Replace(messageTitle, @"[^\u0000-\u00FF]", string.Empty);
+                var body = Regex.Replace(messageBody, @"[^\u0000-\u00FF]", string.Empty);
+                var email = Regex.Replace("soundsofspacetime@gmail.com", @"[^\u0000-\u00FF]", string.Empty);
+                shareurl = "mailto:" + email + "?subject=" + WebUtility.UrlEncode(subject) + "&body=" + WebUtility.UrlEncode(body);
+            }
+            else 
+            {
+                //for Android it is not necessary to code nor is it necessary to assign a destination email
+                shareurl = "mailto:soundsofspacetime@gmail.com?subject=" + messageTitle + "&body=" + messageBody;
+            }
+            Device.OpenUri(new Uri(shareurl));
+        }
 
         #endregion
 
