35fcdfda39c2db89a34c6b18654f4ce35919a959
Nick Provost <nickprovs@hotmail.com>
2019-04-13 22:42:28 -0400

    Added in some useful classes for the future. Playing around with simulator page.

---
+++ Nickprovs.Albatross/Nickprovs.Albatross/Audio/AudioData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nickprovs.Albatross.Audio
+{
+    public class AudioData
+    {
+        public double[] dataPoints;
+        public double tStop;
+        public uint totalPoints;
+
+        public AudioData(double[] userDataPoints, double userTStop, uint userTotalPoints)
+        {
+            dataPoints = userDataPoints;
+            tStop = userTStop;
+            totalPoints = userTotalPoints;
+        }
+    }
+}
