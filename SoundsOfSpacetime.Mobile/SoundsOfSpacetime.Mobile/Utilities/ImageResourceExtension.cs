36a17ecc791c8e07e500143c7969da150e94419a
Nick Provost <nickprovs@hotmail.com>
2020-11-22 23:08:04 -0500

    moved some image resources up to the project level. Messaged around with ios splash screen.

---
+++ SoundsOfSpacetime.Mobile/SoundsOfSpacetime.Mobile/Utilities/ImageResourceExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SoundsOfSpacetime.Mobile.Utilities
+{
+    [ContentProperty (nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            var imageSource = ImageSource.FromResource(this.Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
