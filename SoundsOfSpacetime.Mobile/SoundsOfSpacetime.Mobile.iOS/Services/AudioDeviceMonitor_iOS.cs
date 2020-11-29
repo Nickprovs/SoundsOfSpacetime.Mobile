476b6c51fbe70ffcee7d11e685249c04b2920cae
Nickprovs <provost.nikolai@gmail.com>
2020-11-28 23:40:31 -0500

    headphone monitoring in ios

--- SoundsOfSpacetime.Mobile/SoundsOfSpacetime.Mobile.iOS/Services/AudioDeviceMonitor_iOS.cs
+++ SoundsOfSpacetime.Mobile/SoundsOfSpacetime.Mobile.iOS/Services/AudioDeviceMonitor_iOS.cs
@@ -1,11 +1,16 @@
-﻿using SoundsOfSpacetime.Mobile.Events.Args;
+﻿using AVFoundation;
+using Foundation;
+using SoundsOfSpacetime.Mobile.Events.Args;
 using SoundsOfSpacetime.Mobile.Interfaces;
 using System;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Forms;
 
 namespace SoundsOfSpacetime.Mobile.iOS.Services
 {
-    public class AudioDeviceMonitor_iOS : IAudioDeviceMonitor
+    public class AudioDeviceMonitor_iOS :  IAudioDeviceMonitor
     {
         #region Fields
 
@@ -46,11 +51,37 @@ namespace SoundsOfSpacetime.Mobile.iOS.Services
 
         public AudioDeviceMonitor_iOS()
         {
+            var outputs = AVAudioSession.SharedInstance().CurrentRoute.Outputs;
+
+            //It would be better to hook into the audio session manager but I can't get this to work.
+            //var test= AVAudioSession.SharedInstance().AddObserver("AVAudioSessionRouteChange", NSKeyValueObservingOptions.New, this.OnAudioSessionRouteChange);
+            NSNotificationCenter.DefaultCenter.AddObserver(AVAudioSession.RouteChangeNotification, OnAudioSessionRouteChange);
+
+            this.HeadphonesInUse = this.IsHeadphonesConnected();
+            this.HeadphonesInUseChanged?.Invoke(this, new HeadphoneStatusChangedEventArgs(this.HeadphonesInUse));
         }
 
         #endregion
 
         #region Non Public Methods
+        private bool IsHeadphonesConnected()
+        {
+            var outputs = AVAudioSession.SharedInstance().CurrentRoute.Outputs;
+            foreach(var output in outputs)
+            {
+                if (output.PortType == AVAudioSession.PortBluetoothA2DP ||
+                   output.PortType == AVAudioSession.PortBluetoothHfp ||
+                   output.PortType == AVAudioSession.PortBluetoothLE ||
+                   output.PortType == AVAudioSession.PortHeadphones)
+                    return true;
+            }
+            return false;
+        }
+
+        private void OnAudioSessionRouteChange(NSNotification change)
+        {
+            this.HeadphonesInUse = this.IsHeadphonesConnected();
+        }
 
         private void OnHeadphoneConnectedStatusChanged(App app, bool connected)
         {
